483bacd93b178e2a641b147f9c1fa316a7bba59c
Marek Õunpuu <70217466+maou01@users.noreply.github.com>
2021-08-04 00:41:36 +0300

    feat(*): add frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,33 +14,47 @@ import { FooterComponent } from './footer/footer.component';
 import { PlanComponent } from './plan/plan.component';
 import { MatSliderModule } from '@angular/material/slider';
 
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { TableComponent } from './plan/table/table.component';
+import { ReservationDialogComponent } from './plan/table/reservation-dialog/reservation-dialog.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { HttpClientModule } from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    ContactComponent,
-    FooterComponent,
-    HomepageComponent,
-    MenuComponent,
-    NavbarComponent,
-    PlanComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatNativeDateModule,
-    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AboutComponent,
+		ContactComponent,
+		FooterComponent,
+		HomepageComponent,
+		MenuComponent,
+		NavbarComponent,
+		PlanComponent,
+		TableComponent,
+		ReservationDialogComponent
+	],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatButtonModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+
+    ],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
